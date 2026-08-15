00b91372fe2cdd36aaa89f9bb199f2764116e61d
Sheela Kancharla <rk.sheela@gmail.com>
2026-08-15 19:42:23 +0530

    logging request and response calls with page.on(request and response)

--- tests/BlockAPICallsTest.spec.js
+++ tests/BlockAPICallsTest.spec.js
@@ -5,6 +5,12 @@ test("Blocking JPG,JPEG Calls", async ({ browser }) => {
   const page = await context.newPage();
 
   page.route("**/*.{jpg,jpeg,png}", (route) => route.abort());
+  page.on("request", (request) => {
+    console.log(request.url());
+  });
+  page.on("response", (response) => {
+    console.log(response.status(), response.url());
+  });
 
   await page.goto("https://rahulshettyacademy.com/client");
   await page.locator("#userEmail").fill("rk.sheela@gmail.com");
