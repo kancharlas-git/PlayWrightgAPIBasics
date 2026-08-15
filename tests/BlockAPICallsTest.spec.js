63aaa028730c8cda632a3aeddbd9e55358ff39c3
Sheela Kancharla <rk.sheela@gmail.com>
2026-08-15 19:35:44 +0530

    BlockAPICallsTest

---
+++ tests/BlockAPICallsTest.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+let webContext;
+test("Blocking JPG,JPEG Calls", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  page.route("**/*.{jpg,jpeg,png}", (route) => route.abort());
+
+  await page.goto("https://rahulshettyacademy.com/client");
+  await page.locator("#userEmail").fill("rk.sheela@gmail.com");
+  await page.locator("#userPassword").fill("Vanaja@1");
+  await page.locator("#login").click();
+  await page.waitForLoadState("networkidle");
+
+  await context.storageState({ path: "state.json" });
+  webContext = await browser.newContext({ storageState: "state.json" });
+});
